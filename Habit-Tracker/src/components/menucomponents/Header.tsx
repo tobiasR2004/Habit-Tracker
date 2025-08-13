a005928c17f48107fdda2e54bebafe2dabe3398a
tobiasR2004 <tobi.26romani@gmail.com>
2025-08-13 19:33:23 -0300

    modal alta habitos

--- Habit-Tracker/src/components/menucomponents/Header.tsx
+++ Habit-Tracker/src/components/menucomponents/Header.tsx
@@ -1,38 +1,55 @@
 import { useNavigate, useLocation } from "react-router-dom";
+import { useState } from "react";
+import AltaModal from "../AltaHabitsM";
 
-export default function Header() {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const path = location.pathname;
-    let buttons;
-    const btnClass = 'border-1 text-white cursor-pointer font-bold py-2 px-8 rounded hover:bg-[#ffffff2e] hover:scale-110 origin-bottom-left transition-all duration-150';
 
-    
-    if(path === "/Menu"){
-        buttons = (
-            <>
-            <button className={btnClass} onClick={() => {navigate("/ManageHabits")}}>
-                Manage Habits 
-            </button>
-            <button className={btnClass} onClick={() => {navigate("/")}}>
-                Menu
-            </button>
-            </>
-        );
-    }
-        else if(path === "/ManageHabits"){
-            buttons = (
-                <>
-                <button className={btnClass} onClick={() => {navigate("/Menu")}}>
-                    Back to Home
-                </button>
-                </>
-            );
-        }
-        
-    return (
-        <div className="flex gap-8 w-fit mx-auto p-2 px-20 mb-10 rounded-b-xl justify-center items-center bg-[#4b4b4b79]">
-            {buttons}
-        </div>
+interface HeaderProps {
+    onOpenModal?: () => void;
+  }
+
+
+export default function Header({ onOpenModal }: HeaderProps) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const path = location.pathname;
+  let buttons;
+  const btnClass = "border-1 text-white cursor-pointer font-bold py-2 px-8 rounded hover:bg-[#ffffff2e] hover:scale-110 origin-bottom-left transition-all duration-150";
+
+  if (path === "/Menu") {
+    buttons = (
+      <>
+        <button
+          className={btnClass}
+          onClick={() => {
+            navigate("/ManageHabits");
+          }}
+        >
+          Manage Habits
+        </button>
+
+      </>
     );
+  } else if (path === "/ManageHabits") {
+    buttons = (
+      <>
+        <button
+          className={btnClass}
+          onClick={() => {
+            navigate("/Menu");
+          }}
+        >
+          Back
+        </button>
+        <button className={btnClass} onClick={ onOpenModal }>
+            Add Habit
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <div className="flex gap-8 w-fit mx-auto p-2 px-20 mb-10 rounded-b-xl justify-center items-center bg-[#4b4b4b79]">
+      {buttons}
+    </div>
+  );
 }
--- Habit-Tracker/src/components/menucomponents/ManageHabits.tsx
+++ Habit-Tracker/src/components/menucomponents/ManageHabits.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { CalendarPage }  from '../Calendar';
-import Header from './Header';
-
+import React from "react";
+import { CalendarPage } from "../Calendar";
+import Header from "./Header";
+import AltaModal from "../AltaHabitsM";
 
 export default function ManageHabits() {
-    return (
-        <section>
-             <Header />
-            <div className="bg-[#6060608e] rounded-2xl max-h-[95vh] overflow-auto">
-                <CalendarPage />
-            </div>        
-        </section>
-    );
-}   
+  const [AltaOpen, setAltaOpen] = React.useState(false);
+  return (
+    <section>
+      <Header onOpenModal={() => setAltaOpen(true)} />
+      <div className="bg-[#6060608e] rounded-2xl max-h-[95vh] overflow-auto">
+        <CalendarPage />
+      </div>
+
+      <AltaModal open={AltaOpen} setOpen={setAltaOpen} />
+    </section>
+  );
+}
